185220ddb5ef31f35b77e60bd6f24ddd60a47495
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-01 21:08:18 +0600

    first commit

---
+++ CheckEvenOdd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter Number : ";
+    cin>>x;
+    if(x%2 == 0){
+        cout<<"Your Number is Even \n";
+    }else{
+        cout<<"Your Number is Odd\n";
+    }
+
+    return 0;
+
+}
---
+++ PositiveIneteger.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int x ;
+ cout<<"Enter Positive Integer ";
+  cin>> x;
+  if (x/5 ==0)
+  {
+   cout<<"Divsible by 5" ; 
+  }else
+  {
+    cout<<"Divsible by 5" ; 
+
+  }
+  
+    
+    return 0;
+}
